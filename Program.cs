3dd828f487bfbd838c949323649459cacfd51201
«Sergey» <kane1911@yandex>
2022-07-11 12:08:20 +0300

    Выполнил задачу на определение четное или нечетное число

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача на четное/нечетное
+int a;                             
+            Console.WriteLine("введите число: ");
+            a = Convert.ToInt32(Console.ReadLine()); 
+ 
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("четное число");
+           
+            }
+            else
+            {
+                Console.WriteLine("нечетное");
+            }
